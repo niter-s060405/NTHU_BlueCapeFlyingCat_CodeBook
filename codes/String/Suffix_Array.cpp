f9b9c8863c28cbb15cc9492176bf0b592c2c0883
temmie <kapoo950807@gmail.com>
2025-10-19 00:55:42 +0800

    fix hash value

--- codes/String/Suffix_Array.cpp
+++ codes/String/Suffix_Array.cpp
@@ -3,35 +3,35 @@ struct SuffixArray {
     string s;
     vector<int> sa, lcp;
 
-	// b56519
+    // 0eea96
     template<typename T>
     void sais(const T s, int n, int *sa, int *lar, int *p, int *t, int A) {
         int *rnk = p + n, *q = p + n / 2, *bkt = lar + A;
         int m = 0, i, j, x = t[n - 1] = 1, y = rnk[0] = -1, cnt = -1;
 
-		for (i=n-2 ; ~i ; i--) t[i] = (s[i] == s[i + 1] ? t[i + 1] : s[i] < s[i + 1]);
-		for (i=1 ; i<n ; i++) rnk[i] = t[i] && !t[i-1] ? (p[m]=i, m++) : -1;
+        for (i=n-2 ; ~i ; i--) t[i] = (s[i] == s[i + 1] ? t[i + 1] : s[i] < s[i + 1]);
+        for (i=1 ; i<n ; i++) rnk[i] = t[i] && !t[i-1] ? (p[m]=i, m++) : -1;
         fill_n(lar, A, 0);
-		for (i=0 ; i<n ; i++) ++lar[s[i]];
-		for (i=1 ; i<A ; i++) lar[i] += lar[i-1];
+        for (i=0 ; i<n ; i++) ++lar[s[i]];
+        for (i=1 ; i<A ; i++) lar[i] += lar[i-1];
         auto pushS = [&](int x) { sa[--bkt[s[x]]] = x; };
         auto pushL = [&](int x) { sa[bkt[s[x]]++] = x; };
         auto induce_sort = [&](int* v) {
             fill_n(sa, n, 0);
             copy_n(lar, A, bkt);
-			for (i=m-1 ; ~i ; i--) pushS(v[i]);
+            for (i=m-1 ; ~i ; i--) pushS(v[i]);
             copy_n(lar, A - 1, bkt + 1);
             for (i=0 ; i<n ; i++) if (sa[i] && !t[sa[i] - 1]) pushL(sa[i] - 1);
             copy_n(lar, A, bkt);
             for (i=n-1 ; ~i ; i--) if (sa[i] && t[sa[i] - 1]) pushS(sa[i] - 1);
         };
         induce_sort(p);
-		for (i=0 ; i<n ; i++){
-			if (~(x=rnk[sa[i]])){
-				j = y < 0 || memcmp(s + p[x], s + p[y], (p[x + 1] - p[x]) * sizeof(s[0]));
-				q[y = x] = cnt += j;
-			}
-		}
+        for (i=0 ; i<n ; i++){
+            if (~(x=rnk[sa[i]])){
+                j = y < 0 || memcmp(s + p[x], s + p[y], (p[x + 1] - p[x]) * sizeof(s[0]));
+                q[y = x] = cnt += j;
+            }
+        }
         if (cnt+1<m) sais(q, m, sa, bkt, rnk, t + n, cnt + 1);
         else for (i=0 ; i<m ; i++) sa[q[i]] = i;
         for (i=0 ; i<m ; i++) q[i] = p[sa[i]];
