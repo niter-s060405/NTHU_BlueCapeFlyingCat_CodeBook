d5b18406a430c50151295a805789c68d32ed76ea
temmie <kapoo950807@gmail.com>
2025-11-07 12:27:36 +0800

    fix syntax error

--- codes/Math/Miller_Rabin.cpp
+++ codes/Math/Miller_Rabin.cpp
@@ -1,5 +1,5 @@
 // O(k log^3 n), k = llsprp.size()
-typedef Uint unsigned long long;
+using Uint = unsigned long long;
 Uint modmul(Uint a, Uint b, Uint m) {
     int ret = a*b - m*(Uint)((long double)a*b/m);
     return ret + m*(ret < 0) - m*(ret>=(int)m);
