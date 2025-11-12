a903a56693c019e6343d4df451941a300c555c86
Niter <niter060405@gmail.com>
2025-11-13 07:33:17 +0800

    更新 圓方樹.cpp 的註解

--- codes/Graph/圓方樹.cpp
+++ codes/Graph/圓方樹.cpp
@@ -1,9 +1,9 @@
 /*
-Tested : https://cses.fi/paste/7192deb5028acbcbe90d03/
+Tested : https://cses.fi/paste/09f2afa2bfd9b6a5e92eb8/
 Author : std_abs
 */
 struct BCC_AP { // 0-based, remember to build
-    int n, nbcc; // note for isolated point
+    int n, nbcc; // 注意孤點不會有任何方點連接
     vector<vector<int>> E, F; // id >= n: 方點
     vector<int> pa, dep, low, stk, paf, depf;
     void dfs(int v, int p) {
@@ -23,10 +23,14 @@ struct BCC_AP { // 0-based, remember to build
             } else low[v] = min(low[v], dep[u]);
         }
     }
+    // 判斷原圖上的一個點是不是割點
     bool is_cut(int x) { return F[x].size() != 1; }
+    // 回傳第 id 個 bcc 的每個點 (0-based)
     vector<int> bcc(int id) { return F[id + n]; }
+    // 對於邊 (u, v) 回傳包含它的 bcc id（每條邊都恰好被一個 bcc 包含）
     int bcc_id(int u, int v) { // starts from 0
         return paf[depf[u] < depf[v] ? v : u] - n; }
+    // 計算在新圖上的 depf, paf 陣列，呼叫 bcc_id() 的時候會用到
     void dfs2(int v, int p) {
         depf[v] = ~p ? depf[p] + 1 : 0, paf[v] = p;
         for (int u : F[v]) if (u != p) dfs2(u, v);
