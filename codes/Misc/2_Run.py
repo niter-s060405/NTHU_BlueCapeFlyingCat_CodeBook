ad5a6c2cf1ab326bb697df3b15df333530d7321b
temmie <kapoo950807@gmail.com>
2025-11-13 01:07:20 +0800

    refactor: rewrite with fewer lines

--- codes/Misc/2_Run.py
+++ codes/Misc/2_Run.py
@@ -1,21 +1,10 @@
 from os import *
-
 f = "pA"
-
 while 1:
-    i = input("input: ")
-    system("clear")
-    p = listdir(".")
-    if i != "":
-        f = i
+    i = input("> ") or f; system("clear"); f = i
     print(f"file = {f}")
     if system(f"g++ {f}.cpp -std=c++17 -Wall -Wextra -Wshadow -O2 -D LOCAL -g -fsanitize=undefined,address -o {f}"):
-        print("CE")
-        continue
-
-    for x in sorted(p):
-        if f in x and ".in" in x:
-            print(x)
-            if system(f"./{f} < {x}"):
-                print("RE")
-            print()
+        print("CE"); continue
+    for x in sorted(listdir()):
+        if x.startswith(f) and x.endswith(".in"):
+            print(x); system(f"./{f} < {x}") and print("RE"); print()
