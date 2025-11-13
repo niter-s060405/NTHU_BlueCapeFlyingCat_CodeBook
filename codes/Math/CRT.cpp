c9047af9bb28bf492a6f8d3b0d56f4aea9f98c1e
Niter <niter060405@gmail.com>
2025-11-13 15:44:45 +0800

    Rewrite CRT.cpp

--- codes/Math/CRT.cpp
+++ codes/Math/CRT.cpp
@@ -1,57 +1,35 @@
-// 求出 d = gcd(a,b)，並找出 x, y 使 ax + by = d
-tuple<int, int, int> extgcd(int a, int b){
-    if (!b) return {a, 1, 0};
-    auto [d, x, y] = extgcd(b, a%b);
-    return {d, y, x-a/b*y};
+/*
+Tested :
+    https://open.kattis.com/problems/generalchineseremainder
+    https://adalab.cs.nthu.edu.tw/contest/75/problem/W7-5 (https://adalab.cs.nthu.edu.tw/status/2ea6c14c9a0d8d8b260d699500c2fda8)
+    https://qoj.ac/submission/1712391
+Author :
+    std_abs
+*/
+using ll = __int128;
+//a * p.first + b * p.second = gcd(a, b)
+pair<ll, ll> extgcd(ll a, ll b) {
+    if (b == 0) return {1, 0};
+    auto [y, x] = extgcd(b, a % b);
+    return pair<ll, ll>(x, y - (a / b) * x);
 }
 
-// CRT maybe need use int128
-int CRT_m_coprime(vector<int> &a, vector<int> &m) {
-    int n = a.size(), p = 1, ans = 0;
-    vector<int> M(n), invM(n);
+pair<ll, ll> CRT(ll x1, ll m1, ll x2, ll m2) {
+    ll g = __gcd(m1, m2);
+    if ((x2 - x1) % g) return make_pair(-1, -1);// no sol
+    m1 /= g, m2 /= g;
+    pair<ll, ll> p = extgcd(m1, m2);
+    ll lcm = m1 * m2 * g;
+    ll res = p.first * (x2 - x1) * m1 + x1;
+    // be careful with overflow
+    return make_pair((res % lcm + lcm) % lcm, lcm);
+}
 
-    for (int i=0 ; i<n ; i++) p *= m[i];
-    for (int i=0 ; i<n ; i++){
-        M[i] = p/m[i];
-        auto [d, x, y] = extgcd(M[i], m[i]);
-        invM[i] = x;
-        ans += a[i]*invM[i]*M[i];
-        ans %= p;
+pair<ll, ll> CRT_arr(vector<ll> &a, vector<ll> &m) {
+    pair<ll, ll> res(a[0], m[0]);
+    for (int i = 1; i < a.size(); ++i) {
+        res = CRT(res.first, res.second, a[i], m[i]);
+        if (res.first == -1) break;
     }
-    return (ans%p+p)%p;
-}
-
-// CRT maybe need use int128
-int CRT_m_not_coprime(vector<int> &a, vector<int> &m) {
-    int n = a.size();
-
-    for (int i=1 ; i<n ; i++){
-        int g = __gcd(m[0], m[i]);
-        if ((a[i]-a[0])%g!=0) return -1;
-
-        auto [d, x, y] = extgcd(m[0], m[i]);
-        x = (a[i]-a[0])*x/g;
-
-        a[0] = x*m[0]+a[0];
-        m[0] = m[0]*m[i]/g;
-        a[0] = (a[0]%m[0]+m[0])%m[0];
-    }
-
-    if (a[0]<0) return a[0]+m[0];
-    return a[0];
-}
-
-// ans = a / b (mod m)
-// ans = ret.F + k * ret.S, k is integer
-pair<int, int> div(int a, int b, int m) {
-    int flag = 1;
-    if (a < 0) { a = -a; flag *= -1; }
-    if (b < 0) { b = -b; flag *= -1; }
-    int t = -1, k = -1;
-    int res = extgcd_abc(b, m, a, t, k);
-    if (res == INF) return {INF, INF};
-    m = abs(m / res);
-    t = t * flag;
-    t = (t % m + m) % m;
-    return {t, m};
+    return res;
 }
